4237341f204cc9cf22134a9556a2418832e8ddc4
Moritz Haist <dev@bildstrich.net>
2024-02-10 11:51:46 +0100

    Initial Commit

---
+++ ReShoot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReShoot
+//
+//  Created by Moritz Haist on 10.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReShoot/ReShootApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReShootApp.swift
+//  ReShoot
+//
+//  Created by Moritz Haist on 10.02.24.
+//
+
+import SwiftUI
+
+@main
+struct ReShootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
